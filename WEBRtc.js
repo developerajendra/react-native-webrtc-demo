b855a92cd5385aca39b444b2a4abb0f8bafbddbc
Rajendra <rajendra@Rajendras-MacBook-Pro.local>
2020-05-16 21:03:41 +0530

    everything working web rtc

--- WEBRtc.js
+++ WEBRtc.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import {View} from 'react-native';
+import {
+    View,
+    Button
+} from 'react-native';
 import {
   RTCPeerConnection,
   RTCIceCandidate,
@@ -10,8 +13,11 @@ import {
   mediaDevices
 } from 'react-native-webrtc';
 
+
 export default function WEBRtc (props) {
-  const [localStream, setLocalStream] = useState(undefined)
+  const [localStream, setLocalStream] = useState(undefined);
+
+
   useEffect(function () {
     mediaDevices.getUserMedia({
       audio: true,
@@ -26,10 +32,20 @@ export default function WEBRtc (props) {
       }
     }).then(setLocalStream);
   }, [])
-  console.log('render', localStream && localStream.toURL())
+
+  
+  
+
+
   return (
-    <View>
+    <View style={{flex:1}}>
       {localStream && <RTCView style={{flex:1}} streamURL={localStream.toURL()}/>}
+      <Button
+        // onPress={()=>onPressLearnMore()}
+        title="Front"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+        />
     </View>
   );
 }
@@ -52,6 +68,7 @@ export default function WEBRtc (props) {
 //     View,
 //     Text,
 //     StatusBar,
+//     Button
 //   } from 'react-native';
 // import {
 //     RTCPeerConnection,
@@ -69,64 +86,72 @@ export default function WEBRtc (props) {
 //     // Initial state
 //     state = {
 //         stream: '',
-//         isFront: true
+//         isFront: false
 //     }
 
-//     componentDidMount() {
-        
-
+//     componentWillMount() {
 //         const configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
 //         const pc = new RTCPeerConnection(configuration);
 
-//         let isFront = true;
+//         // let isFront = false;
 //         mediaDevices.enumerateDevices().then(sourceInfos => {
              
-//         console.log('sourceInfos',sourceInfos);
-//         let videoSourceId;
-//         for (let i = 0; i < sourceInfos.length; i++) {
-//             const sourceInfo = sourceInfos[i];
-//             if(sourceInfo.kind == "videoinput" && sourceInfo.facing == (isFront ? "front" : "environment")) {
-//             videoSourceId = sourceInfo.deviceId;
+//             console.log('sourceInfos',sourceInfos);
+//             let videoSourceId;
+//             for (let i = 0; i < sourceInfos.length; i++) {
+//                 const sourceInfo = sourceInfos[i];
+//                 if(sourceInfo.kind == "videoinput" && sourceInfo.facing == (this.state.isFront ? "front" : "environment")) {
+//                     videoSourceId = sourceInfo.deviceId;
+//                 }
 //             }
-//         }
-//         mediaDevices.getUserMedia({
-//             audio: true,
-//             video: {
-//             mandatory: {
-//                 minWidth: 500, // Provide your own width, height and frame rate here
-//                 minHeight: 300,
-//                 minFrameRate: 30
-//             },
-//             facingMode: (isFront ? "user" : "environment"),
-//             optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
-//             }
-//         })
-//         .then(stream => {
-//             // Got stream!
-//             this.setState({ stream: stream.toURL() });
-//         })
-//         .catch(error => {
-//             // Log error
-//         });
+//             mediaDevices.getUserMedia({
+//                 audio: true,
+//                 video: {
+//                 mandatory: {
+//                     minWidth: 500, // Provide your own width, height and frame rate here
+//                     minHeight: 300,
+//                     minFrameRate: 30
+//                 },
+//                 facingMode: (this.state.isFront ? "user" : "environment"),
+//                 optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
+//                 }
+//             })
+//             .then(stream => {
+//                 // Got stream!
+//                 this.setState({ stream: stream.toURL() });
+//             })
+//             .catch(error => {
+//                 // Log error
+//             });
 //         });
 
 //         pc.createOffer().then(desc => {
-//         pc.setLocalDescription(desc).then(() => {
-//             // Send pc.localDescription to peer
-//         });
+//             pc.setLocalDescription(desc).then(() => {
+//                 // Send pc.localDescription to peer
+//             });
 //         });
 
 //         pc.onicecandidate = function (event) {
 //         // send event.candidate to peer
-//         };
- 
-         
+//         };         
+//     }
+
+//     onPressLearnMore = ()=>{
+//         this.setState({ isFront: true });
 //     }
 
 //     render() {
           
 //         return (
+//             <View style={{flex:1}}>
 //                 <RTCView  style={{flex:1}}  objectFit={'contain'} zOrder={1} streamURL={this.state.stream}/>
+//                 <Button
+//                 onPress={()=>this.onPressLearnMore()}
+//                 title="Front"
+//                 color="#841584"
+//                 accessibilityLabel="Learn more about this purple button"
+//                 />
+//             </View>
 //         );
 //     }
 // }
